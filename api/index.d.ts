e9fb03d9d91b63e91005600a6f179eb34b3db43d
Shin <sirapop1998@gmail.com>
2025-10-21 04:53:04 +0700

    Add Vercel ignore file and update configurations; specify Node.js engine version, adjust TypeScript settings, and implement serverless function for API. Introduce new API entry point with body parser configuration.

---
+++ api/index.d.ts
@@ -0,0 +1,8 @@
+import app from "../src/app";
+export default app;
+export declare const config: {
+    api: {
+        bodyParser: boolean;
+    };
+};
+//# sourceMappingURL=index.d.ts.map
--- api/index.ts
+++ api/index.ts
@@ -2,3 +2,10 @@ import app from "../src/app";
 
 // Export the Express app as a Vercel serverless function
 export default app;
+
+// For Vercel serverless functions
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
